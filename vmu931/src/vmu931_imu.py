a0d454bf5942eb28872055ad774e9cc1e17756df
KevinChiu <fsd83811@gmail.com>
2021-02-26 19:53:01 +0800

    vmu931

--- vmu931/src/vmu931_imu.py
+++ vmu931/src/vmu931_imu.py
@@ -29,7 +29,6 @@ g_imu_msg = Imu()
 def VmuAccelerometersFuc():
     
     while 1:
-        print('test')
         vmu_ser = np.zeros((50), dtype=np.int)
         loop = 0
         while 1:
@@ -101,7 +100,7 @@ def VmuGyroscopesFuc():
 ############################################################################
 # vmu Quaternions reaad function
 ############################################################################
-def VmuQuaternionsFuc():
+def VmuQuaternionsFuc(imu_msg):
     
     while 1:
         vmu_ser = np.zeros((50), dtype=np.int)
@@ -173,6 +172,67 @@ def VmuEulerFuc():
 
     return vmu_x, vmu_y, vmu_z
 
+############################################################################
+# vmu read all function
+############################################################################
+def VmuReadAll(imu_msg):
+    
+    while 1:
+        vmu_ser = np.zeros((50), dtype=np.int)
+        loop = 0
+        # while 1:
+        #     try:
+        #         input_data = int(g_ser_vmu.read().encode('hex'), 16)
+        #         # en_data = int(input_data.encode('hex'))
+        #     except:
+        #         input_data = 0
+        #     if(input_data == 0x01):
+        #         vmu_ser[loop] = input_data
+        #         loop += 1
+        #         break
+        while 1:
+            try:
+                input_data = int(g_ser_vmu.read().encode('hex'), 16)
+                # en_data = int(input_data.encode('hex'))
+            except:
+                input_data = 0
+            
+            vmu_ser[loop] = input_data
+            if(vmu_ser[0] == 0x01):
+                loop += 1
+            if(input_data == 0x04):
+                break
+        # if(vmu_ser[2] == ord('q')):
+        #     vmu_w = struct.unpack('>f', bytearray(list(vmu_ser[7:11])))[0]
+        #     vmu_x = struct.unpack('>f', bytearray(list(vmu_ser[11:15])))[0]
+        #     vmu_y = struct.unpack('>f', bytearray(list(vmu_ser[15:19])))[0]
+        #     vmu_z = struct.unpack('>f', bytearray(list(vmu_ser[19:23])))[0]
+        #     if(vmu_w != 0.0 and vmu_x != 0.0 and vmu_y != 0.0 and vmu_z != 0.0):
+        #         break
+        print(vmu_ser[2])
+        if(vmu_ser[2] == ord('q')):
+            imu_msg.orientation.w = struct.unpack('>f', bytearray(list(vmu_ser[7:11])))[0]
+            imu_msg.orientation.x = struct.unpack('>f', bytearray(list(vmu_ser[11:15])))[0]
+            imu_msg.orientation.y = struct.unpack('>f', bytearray(list(vmu_ser[15:19])))[0]
+            imu_msg.orientation.z = struct.unpack('>f', bytearray(list(vmu_ser[19:23])))[0]
+            if(imu_msg.orientation.w != 0.0 and imu_msg.orientation.x != 0.0 and imu_msg.orientation.y != 0.0 and imu_msg.orientation.z != 0.0):
+                break
+        elif(vmu_ser[2] == ord('g')):
+            imu_msg.angular_velocity.x = struct.unpack('>f', bytearray(list(vmu_ser[7:11])))[0]
+            imu_msg.angular_velocity.y = struct.unpack('>f', bytearray(list(vmu_ser[11:15])))[0]
+            imu_msg.angular_velocity.z = struct.unpack('>f', bytearray(list(vmu_ser[15:19])))[0]
+            if(imu_msg.angular_velocity.x != 0.0 and imu_msg.angular_velocity.y != 0.0 and imu_msg.angular_velocity.z != 0.0):
+                break
+        elif(vmu_ser[2] == ord('a')):
+            imu_msg.linear_acceleration.x = struct.unpack('>f', bytearray(list(vmu_ser[7:11])))[0]
+            imu_msg.linear_acceleration.y = struct.unpack('>f', bytearray(list(vmu_ser[11:15])))[0]
+            imu_msg.linear_acceleration.z = struct.unpack('>f', bytearray(list(vmu_ser[15:19])))[0]
+            if(imu_msg.linear_acceleration.x != 0.0 and imu_msg.linear_acceleration.y != 0.0 and imu_msg.linear_acceleration.z != 0.0):
+                break
+
+    # return vmu_w, vmu_x, vmu_y, vmu_z
+    return imu_msg
+
 ##############################################################################
 # vmu talker node
 ###########################################################################
@@ -187,31 +247,32 @@ def vmu_talker():
     rospy.init_node('vmu_talker', anonymous=True)
     # rate = rospy.Rate(100)  # 10hz
     while not rospy.is_shutdown():
-        print("catching data...")
-        acc_x, acc_y, acc_z = VmuAccelerometersFuc()
-        print('acc_x:%.4f acc_y:%.4f acc_z:%.4f'%(acc_x, acc_y, acc_z))
-        gyro_x, gyro_y, gyro_z = VmuGyroscopesFuc()
-        print('gyro_x:%.4f gyro_y:%.4f gyro_z:%.4f'%(gyro_x, gyro_y, gyro_z))
-        quate_w, quate_x, quate_y, quate_z = VmuQuaternionsFuc()
-        print('quate_w:%.4f quate_x:%.4f quate_y:%.4f quate_z:%.4f'%(quate_w, quate_x, quate_y, quate_z))
-        euler_x, euler_y, euler_z = VmuEulerFuc()
-        print('euler_x:%.4f euler_y:%.4f euler_z:%.4f'%(euler_x, euler_y, euler_z))
+        # print("catching data...") #debug
+        # acc_x, acc_y, acc_z = VmuAccelerometersFuc()
+        # print('acc_x:%.4f acc_y:%.4f acc_z:%.4f'%(acc_x, acc_y, acc_z))
+        # gyro_x, gyro_y, gyro_z = VmuGyroscopesFuc()
+        # print('gyro_x:%.4f gyro_y:%.4f gyro_z:%.4f'%(gyro_x, gyro_y, gyro_z))
+        # quate_w, quate_x, quate_y, quate_z = VmuQuaternionsFuc()
+        # print('quate_w:%.4f quate_x:%.4f quate_y:%.4f quate_z:%.4f'%(quate_w, quate_x, quate_y, quate_z))
+        # euler_x, euler_y, euler_z = VmuEulerFuc()
+        # print('euler_x:%.4f euler_y:%.4f euler_z:%.4f'%(euler_x, euler_y, euler_z))
+        g_imu_msg = VmuReadAll(g_imu_msg)
 
-        g_vmu_msg = euler_z
+        # g_vmu_msg = euler_z
         # print(g_vmu_msg)
 
         g_imu_msg.header.stamp = rospy.Time.now()
         g_imu_msg.header.frame_id = "imu_link"
-        g_imu_msg.orientation.x = quate_x
-        g_imu_msg.orientation.y = quate_y
-        g_imu_msg.orientation.z = quate_z
-        g_imu_msg.orientation.w = quate_w
-        g_imu_msg.linear_acceleration.x = acc_x
-        g_imu_msg.linear_acceleration.y = acc_y
-        g_imu_msg.linear_acceleration.z = acc_z
-        g_imu_msg.angular_velocity.x =    gyro_x
-        g_imu_msg.angular_velocity.y =    gyro_y
-        g_imu_msg.angular_velocity.z =    gyro_z
+        # g_imu_msg.orientation.x = quate_x
+        # g_imu_msg.orientation.y = quate_y
+        # g_imu_msg.orientation.z = quate_z
+        # g_imu_msg.orientation.w = quate_w
+        # g_imu_msg.linear_acceleration.x = acc_x
+        # g_imu_msg.linear_acceleration.y = acc_y
+        # g_imu_msg.linear_acceleration.z = acc_z
+        # g_imu_msg.angular_velocity.x =    gyro_x
+        # g_imu_msg.angular_velocity.y =    gyro_y
+        # g_imu_msg.angular_velocity.z =    gyro_z
 
         pub.publish(g_imu_msg)
 
@@ -255,6 +316,8 @@ if __name__ == '__main__':
         while 1:
             continue
     print("serial connect")
+    g_ser_vmu.write("vara")
+    g_ser_vmu.write("varg")
     try:
         vmu_talker()
     except rospy.ROSInterruptException:
